a09818d70d5e821f14b8e11d9371e2a897bb921e
jayasai306 <jayasai306@gmail.com>
2019-03-27 23:29:12 +0530

    second time

--- SmileDetection/src/org/SmileDetector/Smile.java
+++ SmileDetection/src/org/SmileDetector/Smile.java
@@ -1,5 +1,7 @@
 package org.SmileDetector;
 
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCamera2View;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
@@ -10,17 +12,26 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
-public class Smile {
-	public void detectSmile() {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+public class Smile implements CameraBridgeViewBase.CvCameraViewListener2 {
 
+	public JavaCamera2View mCamera = null;
+
+	public void detectSmile() {
+		if (mCamera != null)
+			mCamera.enableView();
+
+	}
+
+	private Mat takePic(Mat img) {
+
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat image = img;
 		CascadeClassifier faceDetector = new CascadeClassifier();
 		faceDetector.load("haarcascade_frontalface_alt.xml");
 
 		CascadeClassifier smileDetector = new CascadeClassifier();
 		smileDetector.load("haarcascade_smile.xml");
 
-		Mat image = Imgcodecs.imread("E:\\input.jpg");
 		Mat gray = new Mat();
 		Imgproc.cvtColor(image, gray, Imgproc.COLOR_RGB2GRAY);
 
@@ -34,11 +45,35 @@ public class Smile {
 		MatOfRect smileDetections = new MatOfRect();
 		smileDetector.detectMultiScale(image, smileDetections);
 
-		for (Rect rect : faceDetections.toArray()) {
-			Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(255, 0, 0));
+		if (smileDetections.toArray().length == 0) {
+			System.out.println("smile please!!!");
+		} else {
+			String filename = "Ouput.jpg";
+			Imgcodecs.imwrite("E:\\" + filename, img);
 		}
 
-		String filename = "Ouput.jpg";
-		Imgcodecs.imwrite("E:\\" + filename, image);
+		return image;
+	}
+
+	@Override
+	public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+		// Get edge from the image
+		Mat result = new Mat();
+		result = inputFrame.rgba();
+
+		// Return result
+		return takePic(result);
+	}
+
+	@Override
+	public void onCameraViewStarted(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onCameraViewStopped() {
+		// TODO Auto-generated method stub
+
 	}
 }
--- SmileDetection/src/org/SmileDetector/SmileDetector.java
+++ SmileDetection/src/org/SmileDetector/SmileDetector.java
@@ -1,7 +1,11 @@
 package org.SmileDetector;
 
+import org.opencv.R;
+import org.opencv.android.JavaCamera2View;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 
 public class SmileDetector extends Activity {
 
@@ -15,7 +19,11 @@ public class SmileDetector extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.camera);
 		init();
+		mSmile.mCamera = (JavaCamera2View) findViewById(R.id.show_camera_activity_java_surface_view);
+		mSmile.mCamera.setVisibility(SurfaceView.VISIBLE);
+		mSmile.mCamera.setCvCameraViewListener(mSmile);
 	}
 
 	/**
